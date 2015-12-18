481d862d69a669decbd14142db9ed023c11990e2
Damian <damian.whyte@thebluehive.com>
2015-12-18 16:44:33 +0000

    image gallery and view change functionallity added

---
+++ Gruntfile.js
@@ -0,0 +1,84 @@
+module.exports = function(grunt) {
+
+    "use strict";
+
+    grunt.loadNpmTasks("grunt-contrib-sass");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-connect");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks('grunt-autoprefixer');
+
+    grunt.initConfig({
+
+        sass: {
+
+            dev: {
+                options: {
+                    style: "compressed"
+                },
+
+                files : {
+                    "styles.min.css": "scss/styles.scss"
+                }
+            }
+        },
+
+        uglify: {
+
+            dev: {
+                options: {
+                    compress: true,
+                    mangle: true,
+                    preserveComments: false
+                },
+
+                files: {
+                    "app.min.js" : ["js/app.js"]
+                }
+            }
+        },
+
+        connect: {
+
+            server : {
+                options: {
+                    open: true
+                }
+            }
+        },
+
+        autoprefixer: {
+          options: {
+            browsers: ['last 8 versions']
+          },
+          dist: { // Target
+            files: {
+              'styles.min.css': 'scss/styles.scss'
+            }
+          }
+        },
+
+        watch: {
+
+            options: {
+                livereload: true,
+            },
+
+            html: {
+                files: ["index.html"],
+            },
+
+            js: {
+                files: ["js/*.js"],
+                tasks: ["uglify:dev"]
+            },
+
+            scss: {
+                files: ["scss/*.scss"],
+                tasks: ["sass:dev", "autoprefixer"]
+            }
+        }
+    });
+
+    grunt.registerTask("run", ["sass:dev", "uglify:dev", "connect:server", "watch"]);
+};
